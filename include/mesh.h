30e5a8401a21dd3c2ec0474e1b547ff7f8c1db11
Nicolas Moignot <nicolas@moignot.net>
2022-02-04 22:53:33 +0100

    Add PLYReader class and Mesh structure

---
+++ include/mesh.h
@@ -0,0 +1,18 @@
+#ifndef MESH_H
+#define MESH_H
+
+struct Mesh
+{
+	float* verticesPosition = nullptr;
+	float* verticesColor = nullptr;
+	int* facesVertices = nullptr;
+	int* facesMaterial = nullptr;
+
+	int nbVertices = 0;
+	int nbFaces = 0;
+
+	bool haveColors = false;
+	bool haveMaterials = false;
+};
+
+#endif // MESH_H
---
+++ include/plyreader.h
@@ -0,0 +1,33 @@
+#ifndef PLYREADER_H
+#define PLYREADER_H
+
+#include <iostream>
+
+#include "mesh.h"
+
+class PLYReader
+{
+public:
+	PLYReader();
+	PLYReader(std::string filepath);
+	PLYReader(PLYReader *reader);
+	~PLYReader();
+
+	bool Load();
+	bool LoadFile(std::string filepath);
+	void CleanMemory();
+
+	bool IsLoaded();
+
+	std::string GetFilepath();
+	Mesh* GetMesh();
+
+private:
+	void SetFilepath(std::string filepath);
+
+	std::string filepath;
+	bool isLoaded = false;
+	Mesh* mesh = nullptr;
+};
+
+#endif // PLYREADER_H
