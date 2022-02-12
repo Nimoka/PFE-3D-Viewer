3842575d1ae21b66758c34fe8e480dc952b7ea55
Nicolas Moignot <nicolas@moignot.net>
2022-02-12 02:21:52 +0100

    Add FileExists() function

--- include/utils.h
+++ include/utils.h
@@ -1,8 +1,12 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <iostream>
+
 namespace Global {
 	extern int nextModuleID;
 };
+
+bool FileExists(std::string path);
 
 #endif // UTILS_H
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,3 +1,14 @@
 #include "utils.h"
 
+#include <fstream>
+
 int Global::nextModuleID = 0;
+
+bool FileExists(std::string path) {
+	std::ifstream file(path.c_str());
+	if (!file.is_open())
+		return false;
+
+	file.close();
+	return true;
+}
