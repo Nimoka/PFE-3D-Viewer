151b609ed5a3117955f64f545d34001083286f1c
Nicolas Moignot <nicolas@moignot.net>
2022-02-12 02:21:19 +0100

    Add TOML parsing

--- src/context.cpp
+++ src/context.cpp
@@ -1,5 +1,7 @@
 #include "context.h"
 
+#include <iostream>
+
 #include "CLI/CLI.hpp"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
@@ -130,7 +132,7 @@ int Context::LoadCLIContext(int argc, char** argv) {
 
 	/* Load default configuration file */
 
-	this->LoadTOMLContext(DATA_DIR "config/");
+	this->LoadTOMLContext(DATA_DIR "configs/default.toml");
 	// TODO: Maybe consider having two differents default configuration files:
 	//       one for viewer mode and another for benchmark mode, and so load
 	//       different files depending on `this->benchmarkMode`.
@@ -167,6 +169,41 @@ int Context::LoadCLIContext(int argc, char** argv) {
 }
 
 bool Context::LoadTOMLContext(std::string filepath) {
+	try {
+		/* Parse TOML file */
+
+		auto data = toml::parse(filepath);
+
+		/* Process options found */
+
+		// Window
+		{
+			auto& window = toml::find(data, "window");
+
+			std::string title = toml::find_or<std::string>(window, "title", "");
+			if (!title.empty())
+				this->SetForcedWindowTitle(title);
+
+			int width = toml::find_or<int>(window, "width", 0);
+			int height = toml::find_or<int>(window, "height", 0);
+			if (width && height) {
+				this->SetWindowSize(width, height);
+			} else if (width) {
+				this->SetWindowSize(width, DEFAULT_WINDOW_HEIGHT);
+			} else if (height) {
+				this->SetWindowSize(DEFAULT_WINDOW_WIDTH, height);
+			}
+		}
+	} catch (const std::runtime_error &e) {
+		std::cerr << e.what() << std::endl;
+		return false;
+	} catch (const toml::syntax_error &e) {
+		std::cerr << "Syntax error found in file ‘" << filepath << "’.";
+		return false;
+	} catch (...) {
+		return false;
+	}
+
 	return true;
 }
 
