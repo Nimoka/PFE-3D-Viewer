04900e2294b632973bbe1b7133df58e76b1bc17d
Nicolas Moignot <nicolas@moignot.net>
2022-02-17 18:15:11 +0100

    Fix viewer module padding

--- src/modules/viewer.cpp
+++ src/modules/viewer.cpp
@@ -36,12 +36,16 @@ void ViewerModule::Render() {
 	ImGui::SetNextWindowPos(viewport->WorkPos);
 	ImGui::SetNextWindowSize(size);
 
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
+
 	ImGui::Begin(std::string(this->title + "###" + std::to_string(this->id)).c_str(), nullptr, this->flags);
 	if (this->renderer != nullptr)
 		ImGui::Image((ImTextureID) this->renderer->GetTextureIDPointer(), size, ImVec2(0, 1), ImVec2(1, 0));
 	else
 		ImGui::Text("No renderer");
 	ImGui::End();
+
+	ImGui::PopStyleVar();
 }
 
 void ViewerModule::SetRenderer(Renderer* renderer) {
