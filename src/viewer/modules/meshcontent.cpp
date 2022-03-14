fab124e93e895198bfafd5d34b76eab7e2827a41
Nimoka <nicolas@moignot.net>
2022-03-14 01:44:31 +0100

    Improve MeshContentModule UI: add collapsing elements for data

--- src/viewer/modules/meshcontent.cpp
+++ src/viewer/modules/meshcontent.cpp
@@ -60,158 +60,167 @@ void MeshContentModule::Render() {
 			ImGui::Separator();
 
 			ImGui::Text("Content:");
-			ImGui::Text("  Vertices:");
-			if (ImGui::BeginTable("Vertices",
-					(this->mesh->HaveColors() ? 7 : 4),
-					this->tableFlags)) {
-				ImGui::TableNextColumn();
-				ImGui::TableNextColumn();
-				ImGui::TableHeader("x");
-				ImGui::TableNextColumn();
-				ImGui::TableHeader("y");
-				ImGui::TableNextColumn();
-				ImGui::TableHeader("z");
-				if (this->mesh->HaveColors()) {
+			if (ImGui::CollapsingHeader("Vertices data")) {
+				if (ImGui::BeginTable("Vertices",
+						(this->mesh->HaveColors() ? 7 : 4),
+						this->tableFlags)) {
 					ImGui::TableNextColumn();
-					ImGui::TableHeader("r");
 					ImGui::TableNextColumn();
-					ImGui::TableHeader("g");
+					ImGui::TableHeader("x");
 					ImGui::TableNextColumn();
-					ImGui::TableHeader("b");
-				}
-
-				unsigned int max = MAX_CONTENT;
-				if (this->mesh->nbVertices < max)
-					max = this->mesh->nbVertices;
-				for (unsigned int i = 0; i < max; i++) {
-					ImGui::TableNextRow();
+					ImGui::TableHeader("y");
 					ImGui::TableNextColumn();
-					ImGui::Text("%u", i);
-					ImGui::TableNextColumn();
-					ImGui::Text("%.2f",
-							this->mesh->verticesData[i].position.x());
-					ImGui::TableNextColumn();
-					ImGui::Text("%.2f",
-							this->mesh->verticesData[i].position.y());
-					ImGui::TableNextColumn();
-					ImGui::Text("%.2f",
-							this->mesh->verticesData[i].position.z());
+					ImGui::TableHeader("z");
 					if (this->mesh->HaveColors()) {
 						ImGui::TableNextColumn();
-						ImGui::Text("%.2f",
-								this->mesh->verticesData[i].color.x());
+						ImGui::TableHeader("r");
+						ImGui::TableNextColumn();
+						ImGui::TableHeader("g");
+						ImGui::TableNextColumn();
+						ImGui::TableHeader("b");
+					}
+
+					unsigned int max = MAX_CONTENT;
+					if (this->mesh->nbVertices < max)
+						max = this->mesh->nbVertices;
+					for (unsigned int i = 0; i < max; i++) {
+						ImGui::TableNextRow();
+						ImGui::TableNextColumn();
+						ImGui::Text("%u", i);
 						ImGui::TableNextColumn();
 						ImGui::Text("%.2f",
-								this->mesh->verticesData[i].color.y());
+								this->mesh->verticesData[i].position.x());
 						ImGui::TableNextColumn();
 						ImGui::Text("%.2f",
-								this->mesh->verticesData[i].color.z());
+								this->mesh->verticesData[i].position.y());
+						ImGui::TableNextColumn();
+						ImGui::Text("%.2f",
+								this->mesh->verticesData[i].position.z());
+						if (this->mesh->HaveColors()) {
+							ImGui::TableNextColumn();
+							ImGui::Text("%.2f",
+									this->mesh->verticesData[i].color.x());
+							ImGui::TableNextColumn();
+							ImGui::Text("%.2f",
+									this->mesh->verticesData[i].color.y());
+							ImGui::TableNextColumn();
+							ImGui::Text("%.2f",
+									this->mesh->verticesData[i].color.z());
+						}
 					}
 				}
-			}
-			ImGui::EndTable();
-			if (this->mesh->nbVertices > MAX_CONTENT)
-				ImGui::Text("(truncated to first %u vertices)", MAX_CONTENT);
-
-			ImGui::Text("  Faces:");
-			if (ImGui::BeginTable("Faces",
-					(this->mesh->HaveMaterials() ? 5 : 4), this->tableFlags)) {
-				ImGui::TableNextColumn();
-				ImGui::TableNextColumn();
-				ImGui::TableHeader("v0");
-				ImGui::TableNextColumn();
-				ImGui::TableHeader("v1");
-				ImGui::TableNextColumn();
-				ImGui::TableHeader("v2");
-				if (this->mesh->HaveMaterials()) {
-					ImGui::TableNextColumn();
-					ImGui::TableHeader("mat");
+				ImGui::EndTable();
+				if (this->mesh->nbVertices > MAX_CONTENT) {
+					ImGui::Text("(truncated to first %u vertices)",
+							MAX_CONTENT);
 				}
+			}
 
-				unsigned int max = MAX_CONTENT;
-				if (this->mesh->nbFaces < max)
-					max = this->mesh->nbFaces;
-				for (unsigned int i = 0; i < max; i++) {
-					ImGui::TableNextRow();
+			if (ImGui::CollapsingHeader("Faces data")) {
+				if (ImGui::BeginTable("Faces",
+						(this->mesh->HaveMaterials() ? 5 : 4),
+						this->tableFlags)) {
 					ImGui::TableNextColumn();
-					ImGui::Text("%u", i);
 					ImGui::TableNextColumn();
-					ImGui::Text("%u",
-							this->mesh->facesVertices[3 * i]);
+					ImGui::TableHeader("v0");
 					ImGui::TableNextColumn();
-					ImGui::Text("%u",
-							this->mesh->facesVertices[3 * i + 1]);
+					ImGui::TableHeader("v1");
 					ImGui::TableNextColumn();
-					ImGui::Text("%u",
-							this->mesh->facesVertices[3 * i + 2]);
+					ImGui::TableHeader("v2");
 					if (this->mesh->HaveMaterials()) {
 						ImGui::TableNextColumn();
+						ImGui::TableHeader("mat");
+					}
+
+					unsigned int max = MAX_CONTENT;
+					if (this->mesh->nbFaces < max)
+						max = this->mesh->nbFaces;
+					for (unsigned int i = 0; i < max; i++) {
+						ImGui::TableNextRow();
+						ImGui::TableNextColumn();
+						ImGui::Text("%u", i);
+						ImGui::TableNextColumn();
 						ImGui::Text("%u",
-								this->mesh->facesMaterials[i]);
+								this->mesh->facesVertices[3 * i]);
+						ImGui::TableNextColumn();
+						ImGui::Text("%u",
+								this->mesh->facesVertices[3 * i + 1]);
+						ImGui::TableNextColumn();
+						ImGui::Text("%u",
+								this->mesh->facesVertices[3 * i + 2]);
+						if (this->mesh->HaveMaterials()) {
+							ImGui::TableNextColumn();
+							ImGui::Text("%u",
+									this->mesh->facesMaterials[i]);
+						}
 					}
 				}
+				ImGui::EndTable();
+				if (this->mesh->nbFaces > MAX_CONTENT)
+					ImGui::Text("(truncated to first %u faces)", MAX_CONTENT);
 			}
-			ImGui::EndTable();
-			if (this->mesh->nbFaces > MAX_CONTENT)
-				ImGui::Text("(truncated to first %u faces)", MAX_CONTENT);
 
 			ImGui::Separator();
 
 			ImGui::Text("Normals:");
-			ImGui::Text("  Vertices' normals:");
-			if (ImGui::BeginTable("Vertices' normals", 4, this->tableFlags)) {
-				ImGui::TableNextColumn();
-				ImGui::TableNextColumn();
-				ImGui::TableHeader("x");
-				ImGui::TableNextColumn();
-				ImGui::TableHeader("y");
-				ImGui::TableNextColumn();
-				ImGui::TableHeader("z");
-
-				unsigned int max = MAX_CONTENT;
-				if (this->mesh->nbVertices < max)
-					max = this->mesh->nbVertices;
-				for (unsigned int i = 0; i < max; i++) {
-					ImGui::TableNextRow();
-					ImGui::TableNextColumn();
-					ImGui::Text("%u", i);
-					ImGui::TableNextColumn();
-					ImGui::Text("%.2f",
-							this->mesh->verticesData[i].normal.x());
-					ImGui::TableNextColumn();
-					ImGui::Text("%.2f",
-							this->mesh->verticesData[i].normal.y());
-					ImGui::TableNextColumn();
-					ImGui::Text("%.2f",
-							this->mesh->verticesData[i].normal.z());
-				}
-			}
-			ImGui::EndTable();
-			if (this->mesh->nbVertices > MAX_CONTENT)
-				ImGui::Text("(truncated to first %u faces)", MAX_CONTENT);
-
-			ImGui::Separator();
-
-			if (this->mesh->HaveMaterials()) {
-				ImGui::Text("Materials:");
-				if (ImGui::BeginTable("Number of faces per material", 2,
+			if (ImGui::CollapsingHeader("Vertices' normals")) {
+				if (ImGui::BeginTable("Vertices' normals", 4,
 						this->tableFlags)) {
 					ImGui::TableNextColumn();
-					ImGui::TableHeader("mat");
 					ImGui::TableNextColumn();
-					ImGui::TableHeader("nb faces");
+					ImGui::TableHeader("x");
+					ImGui::TableNextColumn();
+					ImGui::TableHeader("y");
+					ImGui::TableNextColumn();
+					ImGui::TableHeader("z");
 
-					unsigned char currentMaterial =
-							this->mesh->GetMaterialsRange().min()[0];
-					for (unsigned char i = 0; i < this->mesh->nbMaterials; i++) {
+					unsigned int max = MAX_CONTENT;
+					if (this->mesh->nbVertices < max)
+						max = this->mesh->nbVertices;
+					for (unsigned int i = 0; i < max; i++) {
 						ImGui::TableNextRow();
 						ImGui::TableNextColumn();
-						ImGui::Text("%u", currentMaterial++);
+						ImGui::Text("%u", i);
 						ImGui::TableNextColumn();
-						ImGui::Text("%u", this->mesh->nbFacesPerMaterial[i]);
+						ImGui::Text("%.2f",
+								this->mesh->verticesData[i].normal.x());
+						ImGui::TableNextColumn();
+						ImGui::Text("%.2f",
+								this->mesh->verticesData[i].normal.y());
+						ImGui::TableNextColumn();
+						ImGui::Text("%.2f",
+								this->mesh->verticesData[i].normal.z());
 					}
 				}
 				ImGui::EndTable();
+				if (this->mesh->nbVertices > MAX_CONTENT)
+					ImGui::Text("(truncated to first %u faces)", MAX_CONTENT);
+			}
+
+			if (this->mesh->HaveMaterials()) {
+				ImGui::Separator();
+
+				ImGui::Text("Materials:");
+				if (ImGui::CollapsingHeader("Number of faces per material")) {
+					if (ImGui::BeginTable("Number of faces per material", 2,
+							this->tableFlags)) {
+						ImGui::TableNextColumn();
+						ImGui::TableHeader("mat");
+						ImGui::TableNextColumn();
+						ImGui::TableHeader("nb faces");
+
+						unsigned char currentMaterial =
+								this->mesh->GetMaterialsRange().min()[0];
+						for (unsigned char i = 0; i < this->mesh->nbMaterials; i++) {
+							ImGui::TableNextRow();
+							ImGui::TableNextColumn();
+							ImGui::Text("%u", currentMaterial++);
+							ImGui::TableNextColumn();
+							ImGui::Text("%u", this->mesh->nbFacesPerMaterial[i]);
+						}
+					}
+					ImGui::EndTable();
+				}
 			}
 		}
 		ImGui::End();
