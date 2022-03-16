5499556c06c31494603c0c2d9b1d589e2f522060
Fan JIANG <aubeille@live.fr>
2022-03-16 14:00:03 +0100

    manually compute the fps

--- include/viewer/modules/imguiFPS.h
+++ include/viewer/modules/imguiFPS.h
@@ -14,6 +14,7 @@ public:
 
 private:
 	void ShowFPSWindow();
+	void* context;
 };
 
 #endif // MODULES_IMGUIFPS_H
--- src/viewer/modules/imguiFPS.cpp
+++ src/viewer/modules/imguiFPS.cpp
@@ -1,7 +1,10 @@
 #include "modules/imguiFPS.h"
+#include "context.h"
 
 ImGuiFPSModule::ImGuiFPSModule(void* context)
-		: GUIModule(context) {}
+		: GUIModule(context) {
+			this->context = context;
+		}
 
 ImGuiFPSModule::ImGuiFPSModule(ImGuiFPSModule* module)
 		: GUIModule(module->GetContext()) {}
@@ -9,10 +12,10 @@ ImGuiFPSModule::ImGuiFPSModule(ImGuiFPSModule* module)
 ImGuiFPSModule::~ImGuiFPSModule() {}
 
 void ImGuiFPSModule::ShowFPSWindow(){
+	Context* context =  static_cast<Context*> (this->context);
 	ImGui::Begin("FPS");
-	ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
-			1000.0f / ImGui::GetIO().Framerate,
-			ImGui::GetIO().Framerate);
+	ImGui::Text("Application average %.3f (FPS)",
+			1.0/(context->GetDeltaTime()));
 	ImGui::End();
 }
 
